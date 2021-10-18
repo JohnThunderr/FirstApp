dcf3072d06d2f27127323dc90aefa667bdf155ef
senstersroberts <senstersroberts@gmail.com>
2021-10-18 21:24:44 +0300

    new update by roberto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         String jirb = "Janis Irbe";
         int x = 44;
+        int y = 6;
+        int z = 7;
+        System.out.println(y+z);
         System.out.println(jirb + x);
     }
 }
