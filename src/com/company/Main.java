9ce9a8ef7f0ed2c3ec6fe9b0c33c3038dc0b8552
RenarsDegums <renarsdegums@gmail.com>
2021-10-21 15:41:33 +0300

    atjPull

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         String jirb = "Janis Irbe";
+        // meginu sutit no sava lai veidotu pull request
 
         System.out.println(jirb);
     }
